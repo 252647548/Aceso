68216c379108de72ea142e6035f653ea9df7fee7
wangzhi <wangzhi@mogujie.com>
2017-01-06 10:06:56 +0800

    release 0.0.3

--- src/main/groovy/com/mogujie/instantfix/HotfixTask.groovy
+++ src/main/groovy/com/mogujie/instantfix/HotfixTask.groovy
@@ -1,6 +1,5 @@
 package com.mogujie.instantfix
 
-import com.mogujie.groovy.util.Exec
 import com.mogujie.groovy.util.Log
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
@@ -14,25 +13,28 @@ class HotFixTask extends DefaultTask {
 
     @TaskAction
     void taskExec() {
-        def packageTask = project.tasks.findByName("package${varName}")
-        File apkFile = InstantUtil.getTaskOutApkFile(packageTask)
-        Exec.exec(["adb", "push", apkFile.absolutePath, "/sdcard/reload.apk"], null)
-        Log.logLevel = 3
-        Log.i("----------pc's md5-----------")
-        Exec.exec(["md5", apkFile.absolutePath], null)
-        Log.i("----------adb's md5-----------")
-        Exec.exec(["adb", "shell", "md5", "/sdcard/reload.apk"], null)
-        Log.i "----------restart app-----------"
-        Exec.exec(["adb", "shell", "am", "force-stop", "com.mogujie"], null)
-        Log.logLevel = 2
+        Log.i("package instant fix apk successful!")
+//        def packageTask = project.tasks.findByName("package${varName}")
+//        File apkFile = InstantUtil.getTaskOutApkFile(packageTask)
+//        Exec.exec(["adb", "push", apkFile.absolutePath, "/sdcard/reload.apk"], null)
+//        Log.logLevel = 3
+//        Log.i("----------pc's md5-----------")
+//        Exec.exec(["md5", apkFile.absolutePath], null)
+//        Log.i("----------adb's md5-----------")
+//        Exec.exec(["adb", "shell", "md5", "/sdcard/reload.apk"], null)
+//        Log.i "----------restart app-----------"
+//        Exec.exec(["adb", "shell", "am", "force-stop", "com.mogujie"], null)
+//        Log.logLevel = 2
     }
 
     public static class HotFixAction implements org.gradle.api.Action<HotFixTask> {
         String varName
+
         HotFixAction(String varName) {
 
             this.varName = varName
         }
+
         @Override
         void execute(HotFixTask hotfixTask) {
             def assembleTask = hotfixTask.project.tasks.findByName("assemble" + varName)
